7368c081b9033d5abcd7aa06cd90010dcc90010f
Kyle <kaisonpham@gmail.com>
2026-09-16 21:57:11 -0700

    testing clipping function

--- app/src/engine/core/geometry/Polygon.js
+++ app/src/engine/core/geometry/Polygon.js
@@ -538,7 +538,7 @@ class IntersectionNode {
                 current = current.neighbor;
                 if (current.visited) break;
                 current.visited = true;
-                reverse = !reverse;
+                //reverse = !reverse;
             }
             current = reverse
                 ? current.prev
