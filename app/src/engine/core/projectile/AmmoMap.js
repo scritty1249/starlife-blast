b5717e58fdb51825fd1df076a57cdb561d06c5fe
Kyle <kaisonpham@gmail.com>
2026-09-16 18:40:53 -0700

    Removed debug message

--- app/src/engine/core/projectile/AmmoMap.js
+++ app/src/engine/core/projectile/AmmoMap.js
@@ -71,7 +71,6 @@ export class AmmoMap {
             const count = originalHoleCounts[i];
             poly.holes.splice(count, poly.holes.length - count);
         });
-        console.debug(ammo.blasts);
         return new AmmoMap(finished, time, ammo.legend, ammo.blasts);
     }
     static decode (obj) {
