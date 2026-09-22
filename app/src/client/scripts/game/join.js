4897d67b45c4a51c12861e60ebc449b6b6a99ea8
Kyle <kaisonpham@gmail.com>
2026-09-22 11:05:02 -0700

    Made join lobby notification last indefinitely

--- app/src/client/scripts/game/join.js
+++ app/src/client/scripts/game/join.js
@@ -17,7 +17,7 @@ export default async function init (mainController, Discord, lobby, lobbyid, isH
             mainController.Events.raiseEvent("LOADING", {hide: true});
             if (success) {
                 console.info(`Lobby ${lobbyid} joined`);
-                mainController.Events.raiseEvent("NOTIFY", {severity: 1, message: "Joined lobby."});
+                mainController.Events.raiseEvent("NOTIFY", {severity: 1, message: "Joined lobby.", timeout: -1});
             } else {
                 mainController.Events.raiseEvent("NOTIFY", {severity: -2, message: `Something went wrong while joining the lobby. Close the game and try again in ${(RETRY_MIN_TIMEOUT_MS / 1000).toFixed(1)}s.`, timeout: RETRY_MIN_TIMEOUT_MS + 500});
                 setTimeout(() => phase.setJoinButtonVisibility(true), RETRY_MIN_TIMEOUT_MS);
